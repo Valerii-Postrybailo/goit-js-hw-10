e936973c87f8e397ea4442ee23d4f01bd3867bd3
Valerii-Postrybailo <postribailo.valeri@gmail.com>
2022-09-01 11:01:30 +0300

    houston we got a problem

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,7 +2,7 @@ const BACK_END_URL =  'https://restcountries.com/v3.1/name'
 
 function fetchCountries(name) {
   return
-    fetch(`${BACK_END_URL}/${name}?official,capital,capital,flags.svg,languages`)
+    fetch(`${BACK_END_URL}/${name}?official,capital,flags.svg,languages`)
       .then(response => {
         if (response.status === 404) {
           return Promise.reject(new Error());
--- src/index.js
+++ src/index.js
@@ -2,13 +2,76 @@ import './css/styles.css';
 import debounce from 'lodash.debounce';
 import { fetchCountries } from './fetchCountries';
 
-
 const DEBOUNCE_DELAY = 300;
-
 
 const input = document.querySelector("#search-box")
 console.log(input)
 
-input.addEventListener("input", evt => {
-  console.log(evt.currentTarget.value)
-})
+const listEl = document.querySelector('.country-list');
+const infoEl = document.querySelector('.country-info');
+
+
+const deleteMarkup = ref => {
+  ref.innerHTML = ''
+}
+
+const inputEvtHandler = evt => {
+  const textInputValue = evt.target.value.trim();
+  console.log(textInputValue)
+
+  if (!textInputValue) {
+    deleteMarkup(listEl);
+    deleteMarkup(infoEl);
+    return;
+  }
+
+  fetchCountries(textInputValue)
+    .then(data => {
+      // console.log(data);
+      if (data.length > 10) {
+        console.log('Too many matches found. Please enter a more specific name');
+        return;
+      }
+      renderMarkup(data);
+    })
+    .catch(err => {
+      deleteMarkup(listEl);
+      deleteMarkup(infoEl);
+      console.log('Oops, there is no country with that name');
+    });
+}
+
+input.addEventListener('input', debounce(inputEvtHandler, DEBOUNCE_DELAY));
+
+
+const renderMarkup = data => {
+  if (data.lenght === 1) {
+    deleteMarkup(listEl);
+    const markupInfo = creatMurkupWithInfoAboutCountry(data);
+    infoEl.innerHTML = markupInfo;
+  } else {
+    deleteMarkup(infoEl);
+    const markupList = createListMarkup(data);
+    listEl.innerHTML = markupList;
+  }
+};
+
+const createListMarkup = data => {
+  return data
+    .map(
+      ({ name, flags }) =>
+        `<li><img src="${flags.png}" alt="${name.official}" width="60" height="40">${name.official}</li>`,
+    )
+    .join("");
+};
+
+const creatMurkupWithInfoAboutCountry = data => {
+  return data.map(
+    ({ name, capital, flags, languages }) =>
+      ` <h1><img src="${flags.svg}" alt="${name.official}" width="40" height="40">${
+        name.official
+      }</h1>
+      <p>Capital: ${capital}</p>
+      <p>Languages: ${Object.values(languages)}</p>`,
+  )
+}
